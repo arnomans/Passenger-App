caddd705307fe52e178308a5d379f1ec683c3f1d
arnomans <arnomans@outlook.com>
2023-05-07 18:43:19 +0200

    adding sve button feature

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
-/*let count = 0;
+let count = 0;
 
 let countEL = document.getElementById("count-el");
 
 function increment() {
-    count = count + 1;
+    count += 1;
     countEL.innerText = count;
 };
 
 function save() {
     console.log(count);
-} */
-
-let username = "Arno Mans";
-let message = "You have three new notifications";
-
-console.log(message + " " + username + "!");
+}
