9935fd46bf9e0098b6031402bbba5903c4732caf
arnomans <arnomans@outlook.com>
2023-05-08 23:07:25 +0200

    Count added

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ let count = 0;
 
 let countEL = document.getElementById("count-el");
 
+let saveEl = document.getElementById("save-el");
+
 function increment() {
     count += 1;
     countEL.innerText = count;
 };
 
 function save() {
-    console.log(count);
+    let countDash = count + " - ";
+    saveEl.innerText += countDash;
 }
+
