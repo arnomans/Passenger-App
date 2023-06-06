a6e920bdd65f81c3b6edff7d394a140b817c8bfe
Arno Mans <arnomans@outlook.com>
2023-06-06 22:55:55 +0200

    Save & Display Functions Added

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 let count = 0;
 let countEl = document.getElementById("counter");
+let savedCount
+let displayEl = document.getElementById("displayCount");
 
 function counting() {
     count += 1;
     countEl.innerText = count;
 }
 
+function save() {
+    savedCount = count;
+    count = 0;
+}
+
+function display() {
+    let newCount = savedCount + "-";
+    displayEl.innerText += newCount;
+}
